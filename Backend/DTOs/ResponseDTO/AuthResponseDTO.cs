c1e474a8a3a1ced75afecc1c035b3d989fe8d3a8
AlHakeemAlhassan <alhakeemal123@gmail.com>
2026-05-10 10:43:04 +0300

    Created ResponseDTO for account control

---
+++ Backend/DTOs/ResponseDTO/AuthResponseDTO.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Backend.Models;
+
+public class AuthResponseDTO
+{
+    public string Token { get; set; } = string.Empty;
+    public DateTime ExpiresAt { get; set; }
+    public UserResponseDTO User { get; set; } = null!;
+}
---
+++ Backend/DTOs/ResponseDTO/UserResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Backend.Models;
+
+
+public class UserResponseDTO
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string FullName => $"{FirstName} {LastName}";
+    public string Email { get; set; } = string.Empty;
+    public Guid DepartmentId { get; set; }
+    public string DepartmentName { get; set; } = string.Empty;
+    public string Role { get; set; } = string.Empty;
+}
